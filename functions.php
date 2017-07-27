23baa85976517a542bb90dbdb055853cf290d6c0
Louis Dalligos <zindex14@gmail.com>
2017-07-28 01:32:22 +0800

    header footer adjustments

--- functions.php
+++ functions.php
@@ -44,7 +44,8 @@ function ds_suzuki_setup() {
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
-		'menu-1' => esc_html__( 'Primary', 'ds-suzuki' ),
+		'menu-1' => esc_html__( 'Header', 'ds-suzuki' ),
+		'menu-2' => esc_html__( 'Footer', 'ds-suzuki' ),
 	) );
 
 	/*
@@ -107,6 +108,16 @@ function ds_suzuki_widgets_init() {
 		'description'   => esc_html__( 'Add widgets here.', 'ds-suzuki' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Widgets', 'ds-suzuki' ),
+		'id'            => 'footer-widget-up',
+		'description'   => esc_html__( 'Add widgets here.', 'ds-suzuki' ),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
--- header.php
+++ header.php
@@ -59,7 +59,7 @@
 				<?php
 					wp_nav_menu( array(
 						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
+						'menu_id'        => 'header-menu',
 						'container'      => 'ul',
 						'menu_class' => 'nav navbar-nav'
 					) );
